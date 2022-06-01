969baf2216703048c2a351b2303c4d99941bf44a
David Divinefavour <blazer0022@gmail.com>
2022-06-01 19:42:52 +0100

    minor fixes: added a simple case conversion for the dynamic field 'state' in req params

---
+++ routes/cityRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const getFile = require('../utils/readFile');
+const path = require('path');
+
+router.get('/nigeria/cities/:state', async (req,res)=>{
+     const basePath = '/home/enigma/projects/location-api/src/cities/';
+     const {state} = req.params;
+     const fullPath = path.join(basePath,(state.toLowerCase() + '.json'))
+     const data = await getFile(fullPath);
+
+     if(data){
+          return res.status(200).send({
+               status:200,
+               state: `${state.toUpperCase()} STATE`,
+               cities: data          
+          })
+     }
+     return res
+     .status(404)
+     .send({
+          status:404,
+          message: 'Oops... An error occurred.'
+     })
+     
+})
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const stateRoutes = require('./stateRoutes');
+const cities = require('./cityRoutes');
 
 module.exports = (app)=>{
-     app.use('/api/v1/locations',stateRoutes)
+     app.use('/api/v1/locations',stateRoutes),
+     app.use('/api/v1/locations',cities);
 }
