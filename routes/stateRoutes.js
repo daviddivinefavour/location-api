58067420338ec2fa756f2d4ae08b62dc7f1d8e7f
David Divinefavour <blazer0022@gmail.com>
2022-06-01 20:13:31 +0100

    minor fixes

--- routes/stateRoutes.js
+++ routes/stateRoutes.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 const getFile = require('../utils/readFile');
+const states = require('../src/states/states');
 
 router.get('/', (req,res)=>{
      res.send('Landing page')
 })
 router.get('/nigeria/states',async (req,res)=>{
-     const filePath = '/home/enigma/projects/location-api/src/states/states.json';
-     const data = await getFile(filePath);
-     if(data){
+     // const filePath = '/home/enigma/projects/location-api/src/states/states.json';
+     // const data = await getFile(filePath);
+     if(states){
           return res.status(200).send({
                status:200,
-               data          
+               states          
           })
      }
      return res
---
+++ src/states/states.js
@@ -0,0 +1,154 @@
+const states = [
+     {
+             "id": 1,   
+        "name": "Abia (Umuahia)"
+      },
+      {
+             "id": 2,  
+        "name": "Adamawa (Yola)"
+      },
+      {
+             "id": 3,   
+        "name": "Akwa Ibom (Uyo)"
+      },
+      {
+             "id": 4,   
+        "name": "Anambra (Awka)"
+      },
+      {
+             "id": 5,   
+        "name": "Bauchi (Bauchi)"
+      },
+      {
+             "id": 6,    
+        "name": "Bayelsa (Yenagoa)"
+      },
+      {
+             "id": 7,     
+        "name": "Benue (Makurdi)"
+      },
+      {
+             "id": 8,    
+        "name": "Borno (Maiduguri)"
+      },
+     {
+             "id": 9,     
+        "name": "Cross River (Calabar)"
+      },
+      {
+             "id": 10,  
+        "name": "Delta (Asaba)"
+      },
+      {
+           "id": 11,
+        "name": "Ebonyi (Abakaliki)"
+             },
+      {
+         "id": 12,
+        "name": "Edo (Benin (City)"
+              },
+       {
+           "id": 13,
+        "name": "Ekiti (Ado (Ekiti)"
+               },
+        {
+             "id": 14,
+               "name": "Enugu (Enugu)"
+                },
+         {
+               "id": 15,
+               "name": "Gombe (Gombe)"
+ },
+          {
+                 "id": 16,
+               "name": "Imo (Owerri)"
+  },
+            {
+                    "id": 17, 
+               "name": "Jigawa (Dutse)"
+            },
+             {
+       "id": 18,
+               "name": "Kaduna (Kaduna)"
+     },
+     {
+           "id": 19,
+        "name": "Kano (Kano)"
+  },  
+  {
+           "id": 20,
+        "name": "Katsina (Katsina)"
+  },
+  
+              {
+         "id": 21,
+               "name": "Kebbi (Birnin (Kebbio)"
+      },
+              {
+         "id": 22,
+               "name": "Kogi (Lokoja)"
+      },
+               {
+         "id": 23,
+                      "name": "Kwara (Ilorin)"
+      },
+               {
+           "id": 24,
+                      "name": "Lagos (Ikeja)"
+       },
+                {
+             "id": 25,
+                      "name": "Nasarawa (Lafia)"
+        },
+ {
+             "id": 26,  
+        "name": "Niger (Minna)"
+         },
+  {
+             "id": 27,    
+        "name": "Ogun (Abeokuta)"
+          },
+   {
+             "id": 28,      
+        "name": "Ondo (Akure)"
+           },
+           {
+                    "id": 29, 
+               "name": "Osun (Oshogbo)"
+             },
+    {
+       "id": 30,
+        "name": "Oyo (Ibadan)"
+            },
+     {
+         "id": 31,
+        "name": "Plateau (Jos)"
+             },
+      {
+           "id": 32,
+        "name": "Rivers (Port Harcourt)"
+              },
+       {
+             "id": 33,
+        "name": "Sokoto (Sokoto)"
+ },
+        {
+               "id": 34,
+               "name": "Taraba (Jalingo)"
+  },
+         {
+                 "id": 35,
+               "name": "Yobe (Damaturu)"
+   },
+          {
+                   "id": 36,
+               "name": "Zamfara (Gusau)"
+    },
+      {
+             "id": 36,   
+        "name": "Federal Capital Territory (Abuja)"
+     }
+ 
+ ]
+
+module.exports = states;
